304870c2d0ff7a1ad3616ab8212479157d09d918
Shaun <shauncuier@gmail.com>
2021-03-16 21:14:09 +0600

    Py

--- BMI/bmi.py
+++ BMI/bmi.py
@@ -1,18 +1,18 @@
 weight = float(input())
 height = float(input())
 
-print(height**2)
-print(weight//height**2)
+# print(height**2)
+# print(weight/height**2)
 
-if weight//height**2 < 18.5:
+if weight/height**2 < 18.5:
     print('Underweight')
 
 else:
-    if weight//height**2 <= 18.5 or weight//height<25:
+    if weight/height**2 <= 18.5 or weight/height<25:
         print('Normal')
 
     else:
-        if weight//height**2 <= 25 or weight//height <30:
+        if weight/height**2 <= 25 or weight/height <30:
             print('Overweight')
         else:
             print ('Obesity')
